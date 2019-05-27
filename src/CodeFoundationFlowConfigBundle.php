13e60975561778b4d7fcb0812e1bf3b89c46cf47
Edward Murrell <edward.murrell@codefoundation.com.au>
2019-05-27 17:30:26 +1000

    Add base files for configuration.

---
+++ src/CodeFoundationFlowConfigBundle.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace CodeFoundation\Bundle\FlowConfigBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CodeFoundationFlowConfigBundle extends Bundle
+{
+
+}
