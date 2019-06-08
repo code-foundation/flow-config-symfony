6715f911b4d797e65ce5676210529d77005736b7
Edward Murrell <edward.murrell@codefoundation.com.au>
2019-06-08 20:07:29 +1000

    Clean up bundle definition.

--- src/CodeFoundationFlowConfigBundle.php
+++ src/CodeFoundationFlowConfigBundle.php
@@ -11,31 +11,64 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 class CodeFoundationFlowConfigBundle extends Bundle
 {
     /**
+     * Configure Flow Config Symfony bundle.
+     *
      * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
      */
     public function build(ContainerBuilder $container)
     {
-        $namespace = 'CodeFoundation\FlowConfig\Entity';
-        $mappingDir = $this->getEntityPath() . '/DoctrineMaps';
-
         $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
         if (class_exists($ormCompilerClass)) {
-            $arguments = [$mappingDir, '.orm.xml'];
-            $namespaceMap = [$namespace];
-            $locator   = new Definition('Doctrine\Common\Persistence\Mapping\Driver\DefaultFileLocator', $arguments);
-            $driver    = new Definition('Doctrine\ORM\Mapping\Driver\XmlDriver', [$locator]);
-            $aliasMap = ['FlowControl' => $namespace];
-
-            $container->addCompilerPass(
-
-                new DoctrineOrmMappingsPass($driver, $namespaceMap, [], false, $aliasMap)
-            );
+            $this->addDoctrineMappings($container);
         }
     }
 
-    private function getEntityPath(): string
+    /**
+     * Add FlowConfig Entities to default Entity Manager.
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    private function addDoctrineMappings(ContainerBuilder $container): void
     {
-        $reflector = new \ReflectionClass(\CodeFoundation\FlowConfig\Entity\ConfigItem::class);
+        $baseClass = \CodeFoundation\FlowConfig\Entity\ConfigItem::class;
+        $namespace = 'CodeFoundation\FlowConfig\Entity';
+
+        $mappingDir = $this->getEntityPath($baseClass) . '/DoctrineMaps';
+
+        $namespaceMap = [$namespace];
+        $locator = new Definition(
+            'Doctrine\Common\Persistence\Mapping\Driver\DefaultFileLocator',
+            [$mappingDir, '.orm.xml']
+        );
+        $driver = new Definition(
+            'Doctrine\ORM\Mapping\Driver\XmlDriver',
+            [$locator]
+        );
+        $aliasMap = ['FlowControl' => $namespace];
+
+        $container->addCompilerPass(
+            new DoctrineOrmMappingsPass(
+                $driver,
+                $namespaceMap,
+                [],
+                false,
+                $aliasMap
+            )
+        );
+    }
+
+    /**
+     * Get the full system path to the directory of the supplied class.
+     *
+     * @param string $class
+     *
+     * @return string
+     *
+     * @throws \ReflectionException
+     */
+    private function getEntityPath(string $class): string
+    {
+        $reflector = new \ReflectionClass($class);
         $file = $reflector->getFileName();
         $directory = \realpath(dirname($file));
         return $directory;
